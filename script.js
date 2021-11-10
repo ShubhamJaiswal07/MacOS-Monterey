709b47282cab203a0962b5294172a93a9c45ccad
ShubhamJaiswal07 <89930275+ShubhamJaiswal07@users.noreply.github.com>
2021-11-10 12:57:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+let finder= document.getElementById("finder")[0]
+let find = document.getElementById("find")[0]
+
+finder.addEventListener("click,()=>{
+  console.log("clicked");
+  if(find.style.opacity== "0"){
+    find.style.opacity== "1"
+
+  }
+  else{
+    find.style.opacity== "0"
+  }
+}")
